8053d67a41ff9312bb6d90ff9269b1b1051b9c06
HoChan8253 <nahcohochan@gmail.com>
2025-12-19 15:11:06 +0900

    Fix : Food slider
    체력(Food)바 슬라이더 반영 안됬던 것 수정. 게임오버 이미지 추가.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,17 +14,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject gameOverPanel;
     [SerializeField] private TMP_Text gameOverText;
     
-
     public TurnManager TurnManager { get; set; }
     public BoardManager BoardManager;
     public PlayerController PlayerController;
     
     private int m_FoodAmount = 0;
-    //private Label m_FoodLabel;
     private int m_CurrentLevel = 1;
-
-    //private VisualElement m_GameOverPanel;
-    //private Label m_GameOverMessage;
 
     private void Awake()
     {
@@ -39,14 +34,8 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        // Turn 시스템 초기화
         TurnManager = new TurnManager();
         TurnManager.OnTick += OnTurnHappen;
-
-        // UI 참조
-        //m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
-        //m_GameOverPanel = UIDoc.rootVisualElement.Q<VisualElement>("GameOverPanel");
-        //m_GameOverMessage = m_GameOverPanel.Q<Label>("GameOverMessage");
 
         SoundManager.Instance.Play();
 
@@ -55,12 +44,10 @@ public class GameManager : MonoBehaviour
 
     public void StartNewGame() // 새 게임 시작
     {
-        //m_GameOverPanel.style.visibility = Visibility.Hidden;
         HideGameOver();
 
         m_CurrentLevel = 1;
         m_FoodAmount = 40;
-        //m_FoodLabel.text = "Food : " + m_FoodAmount;
 
         if(foodSlider != null)
         {
@@ -92,6 +79,11 @@ public class GameManager : MonoBehaviour
         if(m_FoodAmount < 0)
         {
             m_FoodAmount = 0;
+        }
+
+        if(foodSlider != null)
+        {
+            foodSlider.value = m_FoodAmount;
         }
 
         if (m_FoodAmount <= 0)
